3ac515ea3472836945bf6779355d3509884e4f2e
pzmcz <169125723+pzmcz@users.noreply.github.com>
2024-06-29 14:35:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const fs = require("fs");
+const { exec } = require('child_process');
 
-module.exports = async function () {
-  console.log("Gello!")
+async function installDiscordJS(callback) {
+  console.log("Installing discord.js...");
+  console.log("Fetching discord.js@latest");
+
+  exec('npm install discord.js@latest', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error installing discord.js: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Error installing discord.js: ${stderr}`);
+      return;
+    }
+    console.log(`discord.js installed successfully.`);
+    if (callback) {
+      callback();
+    }
+  });
 }
+
+module.exports = {
+  installDiscordJS
+};
