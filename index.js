c99a8a46021059346ffef8e0e72a60e51c5a0bca
pzmcz <169125723+pzmcz@users.noreply.github.com>
2024-06-29 14:23:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Works")
+// index.js
+
+// Example code to demonstrate functionality
+console.log('Hello from primsxzx package!');
+
+// Example function
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// Export the function to make it accessible to other modules
+module.exports = {
+  greet
+};
